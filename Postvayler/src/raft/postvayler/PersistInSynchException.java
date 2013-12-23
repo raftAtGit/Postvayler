d4705db194d7028a268ced709b69644fabbb92cc
raft <raft@karga>
2013-12-23 23:53:17 +0200

    updated todo list.

--- Postvayler/src/raft/postvayler/PersistInSynchException.java
+++ Postvayler/src/raft/postvayler/PersistInSynchException.java
@@ -4,6 +4,8 @@ package raft.postvayler;
  * <p>Thrown if a @Persist method is called inside a @Sync method. The call maybe direct or indirect. 
  * This is not allowed and will cause a deadlock.</p>
  * 
+ * TODO relax this, see todo list
+ * 
  * @author r a f t
  */
 public class PersistInSynchException extends RuntimeException {
